fcb5fea74a72b4ddf0a9253c9bceb9e65040e882
ANURAG <anurag.omm11@gmail.com>
2026-08-15 17:46:32 +0530

    Frrontend and localhost

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -49,8 +49,8 @@ class ModelEvaluation:
     def _rename_columns(self,df):
         logging.info("Renaming specific columns and casting to int")
         df = df.rename(columns = {
-            "Vehicle_Age_<1 Year":"Vehicle_Age_lt_1_Year",
-            "Vehicle_Age_>2 Years":"Vehicle_Age_gt_2_Years"
+            "Vehicle_Age_< 1 Year":"Vehicle_Age_lt_1_Year",
+            "Vehicle_Age_> 2 Years":"Vehicle_Age_gt_2_Years"
 
         })
         for col in ["Vehicle_Age_lt_1_Year","Vehicle_Age_gt_2_Years", "Vehicle_Damage_Yes"]:
